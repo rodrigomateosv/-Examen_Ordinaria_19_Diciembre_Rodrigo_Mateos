ff9e1fda3969f26ae0abe392c4b75f7d1ccb1d14
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-12-19 08:34:18 +0000

    3

--- ejercio5.py
+++ ejercio5.py
@@ -1,2 +1,20 @@
 
-def función hollow_triangle(altura)
+import sys
+
+def hollow_triangle(height):
+                  
+
+  if height==0  
+   return []
+    
+  if height==1:
+    return ["#"]
+    
+  if height==2:
+    return ["#", "##", "###"]
+    
+  triangle=["_" * (2 * height - 1)]
+  for i in range(1,height-1):
+    triangle.append("" * (height - i - 1) + "#" + "" * (2 * i - 1) + "#" + "_" * (height - i - 1))
+    
+  
