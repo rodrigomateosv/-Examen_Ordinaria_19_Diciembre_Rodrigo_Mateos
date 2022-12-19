5fec6e492b01e61fa36996c73034cd0424c27bbd
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-12-19 08:26:13 +0000

    5

--- ejercicio4.py
+++ ejercicio4.py
@@ -12,4 +12,9 @@ def do_math(cadena)
     numeros11.sort()
      numeros= []
     for i in range(len(numeros11)):   
-      numeros.append(numeros11[i])
+      numeros.append(numeros11[i]+letras[i])
+
+      cadena= for numero in numeros:
+        cadena=cadena+numero+" "
+        
+    return cadena
